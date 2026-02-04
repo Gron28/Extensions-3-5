92af282a92e40d213902d5a9ac59735ec0448bc5
Felipe Villada <67880760+Gron28@users.noreply.github.com>
2026-02-03 21:12:53 -0300

    removed unnecessary file

--- windows/add-to-startup.ps1
+++
@@ -1,8 +0,0 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$StartupPath = [Environment]::GetFolderPath('Startup')
-$ShortcutPath = Join-Path $StartupPath "ActivationSuite.lnk"
-$Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-$Shortcut.TargetPath = Join-Path $PSScriptRoot "dist\ActivationSuite.exe"
-$Shortcut.WorkingDirectory = Join-Path $PSScriptRoot "dist"
-$Shortcut.Save()
-Write-Host "Startup shortcut created at: $ShortcutPath"
